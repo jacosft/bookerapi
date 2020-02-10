dd64ee22c6b5a695de3388305223dbea388cef36
bodjac <bodjac@gmail.com>
2020-02-10 09:53:50 -0500

    Actuailzado carpeta routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,28 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+Route::get('validate-token', function () {
+    return ['user' => User::where("username", "brettsacuna")->get(),'valid' => true];
+});
+
+Route::middleware(['jwt.verify'])->group(function () {
+    Route::get('/persona/search', 'PersonaController@getpersonaid');
+    Route::get('/rhabitacion/search','ReservaEstanciaHabitacionController@gethabitacionreservaestancia');
+    Route::get('/rhuesped/search','ReservaEstanciaHuespedController@gethuespedreservaestancia');
+    //RUTAS QUE INCLUYEN TODOS LOS MÉTODOS HTTP
+    Route::resource('/', 'UsuarioController');
+    Route::resource('/usuario', 'UsuarioController');
+    Route::resource('/habitacion', 'HabitacionController');
+    Route::resource('/clasehabitacion', 'ClaseHabitacionController');
+    Route::resource('/aerolinea', 'AerolineaController');
+    Route::resource('/reserva', 'ReservaEstanciaController');
+    Route::resource('/persona','PersonaController');
+    Route::resource('/pago','PagoController');
+    Route::resource('/rhabitacion','ReservaEstanciaHabitacionController');
+    Route::resource('/rhuesped','ReservaEstanciaHuespedController');
+    
 });
